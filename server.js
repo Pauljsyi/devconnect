21f3b77b42d9dc3fefbeee66db029afadac8fde9
Paul Yi <pauljsyi@gmail.com>
2021-02-26 23:11:36 -0800

    dependencies

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 5000
 
+app.get('/', (req, res) => {
+    res.send('API RUNNING')
+})
+
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
 })
