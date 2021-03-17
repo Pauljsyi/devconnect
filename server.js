b3b840a1e82e984017160d1f4397e65e97c9afc9
Paul Yi <pauljsyi@gmail.com>
2021-03-16 20:32:28 -0700

    API routes

--- server.js
+++ server.js
@@ -6,11 +6,17 @@ const app = express();
 // Connect to Database
 connectDB();
 
-const PORT = process.env.PORT || 5000
 
 app.get('/', (req, res) => {
     res.send('API RUNNING')
 })
+
+app.use('/api/users', require('./routes/API/users'))
+app.use('/api/auth', require('./routes/API/auth'))
+app.use('/api/profile', require('./routes/API/profile'))
+app.use('/api/posts', require('./routes/API/posts'))
+
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`)
