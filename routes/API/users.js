ace96d793826328cc467419b426f85ddd6323be5
Paul Yi <pauljsyi@gmail.com>
2021-03-16 21:23:15 -0700

    user validation

--- routes/API/users.js
+++ routes/API/users.js
@@ -1,18 +1,42 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check')
+const { check, validationResult } = require('express-validator')
 
+const User = require('../../models/User')
 
 //register user
-router.post('/', [
+router.post('/', 
+[
     check('name', 'Name is required').not().isEmpty(),
     check('email', 'Please include a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
-] ,(req, res) => {
+],
+async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()){
         return res.status(400).json({ errors: errors.array() })
     }
+
+    const { name, email, password } = req.body;
+
+    try {
+    //See if user exists
+        let user = await User.findOne({ email })
+
+        if(user) {
+            res.status(400).json({ errors: [{ msg: 'User already exists' }] })
+        }
+    //Get Users gravatar
+
+    //Encrypt password using bcrypt
+
+    //Return jsonwebtoken
+
+    } catch(e) {
+        console.error(e.message);
+        res.status(500).send('Server Error')
+    }
+
     
     res.send('User route')
 })
