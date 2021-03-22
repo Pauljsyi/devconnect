1833c9048d135a6d2b9784692a7649182c348fae
Paul Yi <pauljsyi@gmail.com>
2021-03-21 18:39:19 -0700

    profile validation test

--- routes/API/profile.js
+++ routes/API/profile.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const { errorFromList } = require('verror');
 const router = express.Router();
 const auth = require('../../middleware/auth')
 const Profile = require('../../models/Profile')
 const User = require('../../models/User')
+const { check, validationResult } = require('express-validator')
 
 
 // get request api/profile/me
@@ -22,5 +24,19 @@ router.get('/me', auth, async (req, res) => {
         res.status(500).send('Server Error')
     }
 })
+
+// post request api/profile/me
+// create or update user profile
+// private
+router.post('/', [ auth, [
+    check('status', 'Status is required').not().isEmpty(), 
+    check('skills', 'Skills is required').not().isEmpty()
+]],
+async (req, res) => {
+    const error = validationResult(req);
+    if(!errorFromList.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+});
 
 module.exports = router;
