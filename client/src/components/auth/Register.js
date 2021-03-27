c6465d045ff8fd25c323ea9333d27e0993050d97
Paul Yi <pauljsyi@gmail.com>
2021-03-26 22:31:22 -0700

    register user

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,10 +3,11 @@ import { Link } from 'react-router-dom'
 import axios from 'axios';
 import { connect } from 'react-redux';
 import {setAlert} from '../../actions/alert'
+import {register} from '../../actions/auth'
 import PropTypes from 'prop-types';
 
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
   const [formData, setFormData] = useState({
     name:'',
     email:'',
@@ -24,7 +25,7 @@ const Register = ({ setAlert }) => {
       setAlert('Passwords do not match', 'danger')
     } else {
 
-      console.log('SUCCESS')
+      register({ name, email, password });
       // const newUser = {
       //   name,
       //   email,
@@ -91,7 +92,8 @@ const Register = ({ setAlert }) => {
 }
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
 }
 
-export default connect(null, { setAlert })(Register)
+export default connect(null, { setAlert, register })(Register)
